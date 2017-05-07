1b1ca15d5be156abcc9110d2038de158307bad75
Manish Jat <manish.jat@olacabs.com>
2017-05-07 22:34:04 +0530

    fix syntax error

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import UserComponent from './components/usercomponent';
-import 
-
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Welcome to React</h2>
+          <h2>User database</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <br/>
         <UserComponent/>
       </div>
     );
